3018ff785d16e9f5bec926426a23f68576769a61
zhongxunking <zhongxunking@163.com>
2017-12-20 20:22:00 +0800

    QueryParamsParser使用Cache

--- src/main/java/org/antframework/common/util/jpa/query/annotation/QueryParamsParser.java
+++ src/main/java/org/antframework/common/util/jpa/query/annotation/QueryParamsParser.java
@@ -9,6 +9,7 @@
 package org.antframework.common.util.jpa.query.annotation;
 
 import org.antframework.common.util.jpa.query.QueryParam;
+import org.antframework.common.util.other.Cache;
 import org.springframework.cglib.core.ReflectUtils;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.util.ReflectionUtils;
@@ -16,15 +17,18 @@ import org.springframework.util.ReflectionUtils;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 查询参数解析器
  */
 public class QueryParamsParser {
     // 缓存（每种类型只会在第一次执行时才会进行解析）
-    private static final Map<Class, List<QueryParamParser>> CACHE = new ConcurrentHashMap<>();
+    private static final Cache<Class, List<QueryParamParser>> CACHE = new Cache<>(new Cache.Supplier<Class, List<QueryParamParser>>() {
+        @Override
+        public List<QueryParamParser> get(Class key) {
+            return parseToFieldParser(key);
+        }
+    });
 
     /**
      * 解析出查询参数
@@ -34,30 +38,13 @@ public class QueryParamsParser {
      */
     public static List<QueryParam> parse(Object obj) {
         List<QueryParam> queryParams = new ArrayList<>();
-        for (QueryParamParser fieldParser : getFieldParsers(obj.getClass())) {
+        for (QueryParamParser fieldParser : CACHE.get(obj.getClass())) {
             QueryParam queryParam = fieldParser.parse(obj);
             if (queryParam != null) {
                 queryParams.add(queryParam);
             }
         }
         return queryParams;
-    }
-
-    // 获取字段解析器
-    private static List<QueryParamParser> getFieldParsers(Class clazz) {
-        List<QueryParamParser> fieldParsers = CACHE.get(clazz);
-        if (fieldParsers == null) {
-            synchronized (CACHE) {
-                fieldParsers = CACHE.get(clazz);
-                if (fieldParsers == null) {
-                    // 解析
-                    fieldParsers = parseToFieldParser(clazz);
-                    // 缓存
-                    CACHE.put(clazz, fieldParsers);
-                }
-            }
-        }
-        return fieldParsers;
     }
 
     // 解析出字段解析器
