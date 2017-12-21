11abbfafbf8bdf437db3e664d7bdaed9d4785cdf
zhongxunking <zhongxunking@163.com>
2017-12-21 14:43:11 +0800

    新增EmptyOrder、EmptyResult

---
+++ src/main/java/org/antframework/common/util/facade/EmptyOrder.java
@@ -0,0 +1,15 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-12-21 14:34 创建
+ */
+package org.antframework.common.util.facade;
+
+/**
+ * 空order（对于一些触发性操作，请求中并不需要带信息，则可以使用本类）
+ */
+public class EmptyOrder extends AbstractOrder {
+}
---
+++ src/main/java/org/antframework/common/util/facade/EmptyResult.java
@@ -0,0 +1,15 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-12-21 14:28 创建
+ */
+package org.antframework.common.util.facade;
+
+/**
+ * 空result（对于一些写操作只需要返回是否成功，而不需要返回其他信息，则可以使用本类型）
+ */
+public class EmptyResult extends AbstractResult {
+}
