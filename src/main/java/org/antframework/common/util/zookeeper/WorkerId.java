b78e4ee8939686ea66634cf4706364b18229171e
zhongxunking <zhongxunking@163.com>
2018-01-14 16:58:27 +0800

    新增WorkId

---
+++ src/main/java/org/antframework/common/util/zookeeper/WorkerId.java
@@ -0,0 +1,77 @@
+/*
+ * 作者：钟勋 (e-mail:zhongxunking@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2018-01-13 12:54 创建
+ */
+package org.antframework.common.util.zookeeper;
+
+import org.antframework.common.util.file.MapFile;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.zookeeper.CreateMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * workerId
+ */
+public class WorkerId {
+    private static final Logger logger = LoggerFactory.getLogger(WorkerId.class);
+
+    /**
+     * 获取workerId（本方法不适合被频繁调用，因为每次调用都会连接zookeeper并释放）
+     *
+     * @param worker        worker（每个worker都不一样）
+     * @param zkUrls        zookeeper链接
+     * @param nodePath      worker在zookeeper上注册节点的父路径
+     * @param cacheFilePath 缓存文件路径（null表示不使用缓存文件）
+     * @return workerId
+     */
+    public static int getId(String worker, String[] zkUrls, String nodePath, String cacheFilePath) {
+        MapFile cacheFile = cacheFilePath == null ? null : new MapFile(cacheFilePath);
+        String id;
+        boolean fromZk = true;
+        try {
+            id = getIdFromZk(worker, zkUrls, nodePath);
+        } catch (Throwable e) {
+            logger.error("从zookeeper上读取workerId出错:{}", e);
+            if (cacheFile != null) {
+                logger.warn("尝试从缓存文件读取workerId");
+                id = cacheFile.read(worker);
+                if (id == null) {
+                    throw new IllegalStateException("不存在缓存文件或缓存文件内无workerId");
+                }
+                fromZk = false;
+            } else {
+                throw e;
+            }
+        }
+        if (fromZk && cacheFile != null) {
+            cacheFile.store(worker, id);
+        }
+        return Integer.parseInt(id);
+    }
+
+    // 从zookeeper获取id
+    private static String getIdFromZk(String worker, String[] zkUrls, String nodePath) {
+        ZkTemplate zkTemplate = ZkTemplate.create(zkUrls, nodePath);
+        try {
+            List<String> nodes = zkTemplate.findChildren(ZkTemplate.buildPath(), String.format("^%s-[0-9]+$", worker));
+            if (nodes.size() <= 0) {
+                zkTemplate.createNode(ZkTemplate.buildPath(worker + "-"), CreateMode.PERSISTENT_SEQUENTIAL);
+                nodes = zkTemplate.findChildren(ZkTemplate.buildPath(), String.format("^%s-[0-9]+$", worker));
+            }
+            if (nodes.size() != 1) {
+                throw new IllegalStateException(String.format("worker[%s]在zookeeper上注册的数量[%d]不为1", worker, nodes.size()));
+            }
+            String[] parts = StringUtils.split(nodes.get(0), '-');
+            return parts[parts.length - 1];
+        } finally {
+            zkTemplate.close();
+        }
+    }
+}
