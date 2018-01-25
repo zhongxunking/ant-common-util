7ff7069880291ec79c61fd74bc00e3bef58801f7
zhongxunking <zhongxunking@163.com>
2018-01-25 16:18:56 +0800

    Cache可以缓存null

--- src/test/java/org/antframework/common/util/other/CacheTest.java
+++ src/test/java/org/antframework/common/util/other/CacheTest.java
@@ -18,7 +18,11 @@ public class CacheTest {
     private Cache<String, Integer> cache = new Cache<>(new Cache.Supplier<String, Integer>() {
         @Override
         public Integer get(String key) {
-            return Integer.parseInt(key);
+            Integer value = Integer.parseInt(key);
+            if (value > 100) {
+                return null;
+            }
+            return value;
         }
     });
 
@@ -32,5 +36,10 @@ public class CacheTest {
             int value = cache.get(Integer.toString(i));
             Assert.assertEquals(i, value);
         }
+
+        Integer value1 = cache.get(Integer.toString(101));
+        Assert.assertEquals(null, value1);
+        Integer value2 = cache.get(Integer.toString(101));
+        Assert.assertEquals(null, value2);
     }
 }
