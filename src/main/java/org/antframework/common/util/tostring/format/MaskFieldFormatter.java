8e71c506a5f47f29128ed543187d47e7353e2bb4
zhongxunking <zhongxunking@163.com>
2020-02-01 14:48:24 +0800

    优化@Mask

--- src/main/java/org/antframework/common/util/tostring/format/MaskFieldFormatter.java
+++ src/main/java/org/antframework/common/util/tostring/format/MaskFieldFormatter.java
@@ -8,8 +8,10 @@
  */
 package org.antframework.common.util.tostring.format;
 
+import org.antframework.common.util.other.Cache;
 import org.antframework.common.util.tostring.FieldFormatter;
 import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
@@ -20,6 +22,8 @@ import java.lang.reflect.Field;
 public class MaskFieldFormatter implements FieldFormatter {
     // 全部掩码的字符长度
     private static final int ALL_MASK_STR_SIZE = 6;
+    // 安全掩码缓存
+    private static final Cache<Character, String> SECURE_MASK_CACHE = new Cache<>(MaskFieldFormatter::buildSecureMaskedStr);
 
     // 需被掩码的属性
     private Field field;
@@ -33,39 +37,27 @@ public class MaskFieldFormatter implements FieldFormatter {
     private int endSize;
     // 掩码字符
     private char maskChar;
-    // 安全掩码的字符串
-    private String secureMaskedStr;
 
     @Override
     public void initialize(Field field) {
-        if (field.getType() != String.class) {
-            throw new IllegalArgumentException("@Mask只能标注在String类型字段上，" + field + "不是String类型");
-        }
-
+        Mask maskAnnotation = AnnotatedElementUtils.findMergedAnnotation(field, Mask.class);
+        Assert.isAssignable(CharSequence.class, field.getType(), String.format("@Mask只能标注在CharSequence类型字段上，字段[%s]不是CharSequence类型", field));
+        Assert.isTrue(maskAnnotation.startSize() >= 0 && maskAnnotation.endSize() >= 0, String.format("属性[%s]的@Mask注解设置不合法：startSize、endSize不能小于0", field));
         this.field = field;
         formattedPre = field.getName() + "=";
-        Mask maskAnnotation = AnnotatedElementUtils.findMergedAnnotation(field, Mask.class);
         secureMask = maskAnnotation.secureMask();
         startSize = maskAnnotation.startSize();
         endSize = maskAnnotation.endSize();
         maskChar = maskAnnotation.maskChar();
-        if (startSize < 0 || endSize < 0) {
-            throw new IllegalArgumentException("属性" + field + "的@Mask注解设置不合法：startSize、endSize不能小于0");
-        }
-        if (secureMask) {
-            secureMaskedStr = buildSecureMaskedStr(maskChar);
-        }
     }
 
     @Override
     public String format(Object obj) {
-        String maskedStr;
+        String maskedStr = null;
         String str = (String) ReflectionUtils.getField(field, obj);
-        if (str == null) {
-            maskedStr = null;
-        } else {
+        if (str != null) {
             if (secureMask) {
-                maskedStr = secureMaskedStr;
+                maskedStr = SECURE_MASK_CACHE.get(maskChar);
             } else {
                 maskedStr = MaskUtils.mask(str, startSize, endSize, maskChar);
             }
@@ -75,7 +67,7 @@ public class MaskFieldFormatter implements FieldFormatter {
     }
 
     // 构建固定长度的安全掩码字符串
-    private static String buildSecureMaskedStr(char maskChar) {
+    private static String buildSecureMaskedStr(Character maskChar) {
         StringBuilder builder = new StringBuilder(ALL_MASK_STR_SIZE);
         for (int i = 0; i < ALL_MASK_STR_SIZE; i++) {
             builder.append(maskChar);
