ecd9a3f98893c6dc97c4fecbabbc0d26c4f81dd6
zhongxun <zhongxunking@163.com>
2023-04-18 16:38:12 +0800

    Period增加subtract方法

--- src/test/java/org/antframework/common/util/id/PeriodTest.java
+++ src/test/java/org/antframework/common/util/id/PeriodTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * 作者：钟勋 (e-mail:zhongxunking@163.com)
  */
 
@@ -19,25 +19,69 @@ import java.util.Date;
  * 周期单元测试
  */
 public class PeriodTest {
-    private Date testDate = DateUtils.parseDate("2017-11-22 23:48:21.250", "yyyy-MM-dd HH:mm:ss.SSS");
+    @Test
+    public void testParsePeriod() throws ParseException {
+        Date testDate = parseDate("2017-11-22 23:48:21.250");
 
-    public PeriodTest() throws ParseException {
+        Date hourDate = parseDate("2017-11-22 23:00:00.000");
+        Assert.assertEquals(hourDate, new Period(PeriodType.HOUR, testDate).getDate());
+
+        Date dayDate = parseDate("2017-11-22 00:00:00.000");
+        Assert.assertEquals(dayDate, new Period(PeriodType.DAY, testDate).getDate());
+
+        Date monthDate = parseDate("2017-11-01 00:00:00.000");
+        Assert.assertEquals(monthDate, new Period(PeriodType.MONTH, testDate).getDate());
+
+        Date yearDate = parseDate("2017-01-01 00:00:00.000");
+        Assert.assertEquals(yearDate, new Period(PeriodType.YEAR, testDate).getDate());
+
+        Assert.assertNull(new Period(PeriodType.NONE, testDate).getDate());
     }
 
     @Test
-    public void testParsePeriod() throws ParseException {
-        Date hourDate = DateUtils.parseDate("2017-11-22 23:00:00.000", "yyyy-MM-dd HH:mm:ss.SSS");
-        Assert.assertEquals(hourDate, new Period(PeriodType.HOUR, testDate).getDate());
+    public void testSubtract() throws ParseException {
+        Period hour1 = new Period(PeriodType.HOUR, parseDate("2023-04-18 16:00:00.000"));
+        Period hour2 = new Period(PeriodType.HOUR, parseDate("2023-04-18 16:00:00.000"));
+        Period hour3 = new Period(PeriodType.HOUR, parseDate("2023-04-19 20:00:00.000"));
+        int gap = hour1.subtract(hour2);
+        Assert.assertEquals(gap, 0);
+        gap = hour1.subtract(hour3);
+        Assert.assertEquals(gap, -28);
+        gap = hour3.subtract(hour1);
+        Assert.assertEquals(gap, 28);
 
-        Date dayDate = DateUtils.parseDate("2017-11-22 00:00:00.000", "yyyy-MM-dd HH:mm:ss.SSS");
-        Assert.assertEquals(dayDate, new Period(PeriodType.DAY, testDate).getDate());
+        Period day1 = new Period(PeriodType.DAY, parseDate("2023-04-18 00:00:00.000"));
+        Period day2 = new Period(PeriodType.DAY, parseDate("2023-04-18 00:00:00.000"));
+        Period day3 = new Period(PeriodType.DAY, parseDate("2023-05-25 00:00:00.000"));
+        gap = day1.subtract(day2);
+        Assert.assertEquals(gap, 0);
+        gap = day1.subtract(day3);
+        Assert.assertEquals(gap, -37);
+        gap = day3.subtract(day1);
+        Assert.assertEquals(gap, 37);
 
-        Date monthDate = DateUtils.parseDate("2017-11-01 00:00:00.000", "yyyy-MM-dd HH:mm:ss.SSS");
-        Assert.assertEquals(monthDate, new Period(PeriodType.MONTH, testDate).getDate());
+        Period month1 = new Period(PeriodType.MONTH, parseDate("2023-04-01 00:00:00.000"));
+        Period month2 = new Period(PeriodType.MONTH, parseDate("2023-04-01 00:00:00.000"));
+        Period month3 = new Period(PeriodType.MONTH, parseDate("2024-05-01 00:00:00.000"));
+        gap = month1.subtract(month2);
+        Assert.assertEquals(gap, 0);
+        gap = month1.subtract(month3);
+        Assert.assertEquals(gap, -13);
+        gap = month3.subtract(month1);
+        Assert.assertEquals(gap, 13);
 
-        Date yearDate = DateUtils.parseDate("2017-01-01 00:00:00.000", "yyyy-MM-dd HH:mm:ss.SSS");
-        Assert.assertEquals(yearDate, new Period(PeriodType.YEAR, testDate).getDate());
+        Period year1 = new Period(PeriodType.YEAR, parseDate("2023-01-01 00:00:00.000"));
+        Period year2 = new Period(PeriodType.YEAR, parseDate("2023-01-01 00:00:00.000"));
+        Period year3 = new Period(PeriodType.YEAR, parseDate("2034-01-01 00:00:00.000"));
+        gap = year1.subtract(year2);
+        Assert.assertEquals(gap, 0);
+        gap = year1.subtract(year3);
+        Assert.assertEquals(gap, -11);
+        gap = year3.subtract(year1);
+        Assert.assertEquals(gap, 11);
+    }
 
-        Assert.assertEquals(null, new Period(PeriodType.NONE, testDate).getDate());
+    private static Date parseDate(String date) throws ParseException {
+        return DateUtils.parseDate(date, "yyyy-MM-dd HH:mm:ss.SSS");
     }
 }
