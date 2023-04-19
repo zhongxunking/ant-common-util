3b24abfa7d5abafdb5657d87cc187b567e36e1ca
zhongxun <zhongxunking@163.com>
2023-04-19 16:51:29 +0800

    Exceptions增加Callback入参

--- src/main/java/org/antframework/common/util/kit/Exceptions.java
+++ src/main/java/org/antframework/common/util/kit/Exceptions.java
@@ -15,17 +15,30 @@ import java.util.concurrent.Callable;
  */
 public final class Exceptions {
     /**
-     * 调用接口（如有异常，则直接抛出）
+     * 调用Callable（如有异常，则直接抛出）
      *
-     * @param callable 需调用的接口
+     * @param callable 需调用的Callable
      * @param <T>      返回值类型
-     * @return 接口返回结果
+     * @return Callable返回结果
      */
     public static <T> T call(Callable<T> callable) {
         try {
             return callable.call();
         } catch (Exception e) {
             return rethrow(e);
+        }
+    }
+
+    /**
+     * 调用回调（如有异常，则直接抛出）
+     *
+     * @param callback 回调
+     */
+    public static void call(Callback callback) {
+        try {
+            callback.call();
+        } catch (Throwable e) {
+            rethrow(e);
         }
     }
 
@@ -43,5 +56,17 @@ public final class Exceptions {
     // 异常类型抹除
     private static <T, E extends Throwable> T typeErasure(Throwable e) throws E {
         throw (E) e;
+    }
+
+    /**
+     * 回调
+     */
+    public interface Callback {
+        /**
+         * 调用
+         *
+         * @throws Throwable 发生异常
+         */
+        void call() throws Throwable;
     }
 }
